627c24d226dc61924460a95f29d296c6dc16883a
Ashon <ashon8813@gmail.com>
2015-06-22 22:48:58 +0900

    fix

--- src/som_integration.py
+++ src/som_integration.py
@@ -4,26 +4,17 @@ import time
 from som import Som
 from som import FeatureMap
 import som_util
-<<<<<<< HEAD
-=======
 import sys
 import numpy as np
->>>>>>> refactoring
 
 def main():
 
     width = 20
     height = 20
     som_info = {
-<<<<<<< HEAD
-        'width': 20,
-        'height': 20,
-        'dimension': 10000,
-=======
         'width': width,
         'height': height,
         'dimension': 100,
->>>>>>> refactoring
         'randomize': True,
         'gain': 50,
         'max_iteration': 100,
@@ -33,11 +24,7 @@ def main():
     ])
     som = Som(**som_info)
 
-<<<<<<< HEAD
-    sample_length = 50
-=======
     sample_length = 20
->>>>>>> refactoring
     print 'Initialize Samples [sample_length=%s]' % sample_length
     sample_map = FeatureMap(
         width=sample_length, height=1,
@@ -58,19 +45,11 @@ def main():
 
         # bmu_idx_list = [som.get_bmu_coord(unit) for unit in sample_map.map]
 
-<<<<<<< HEAD
-        print '\nTrain Result [progress={progress:.3f} %] [exec_time={exec_time:.3f} sec]'.format(
-            progress=som.get_progress() * 100, exec_time=train_execution_time)
-
-        print 'Sample idx |', ' | '.join(['{sid:3d}'.format(sid=i) for i in range(sample_length)])
-        print 'BMU idx    |', ' | '.join(['{bid:3d}'.format(bid=bid) for bid in bmu_idx_list])
-=======
         print 'Train Result [progress={progress:.3f} %] [exec_time={exec_time:.3f} sec]'.format(
             progress=som.get_progress() * 100, exec_time=train_execution_time)
 
         # print 'Sample idx |', ' | '.join(['{sid:3d}'.format(sid=i) for i in range(sample_length)])
         # print 'BMU idx    |', ' | '.join(['{bid}'.format(bid=(str(bid[0]) + '-' + str(bid[1]))) for bid in bmu_idx_list])
->>>>>>> refactoring
 
     print 'Train Complete.'
     print 'Total Exec [{exec_total:.3f} sec]'.format(exec_total=total_time)
